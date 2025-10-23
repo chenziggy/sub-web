05add7f47fba4c4aee1dd0cf8170536077c513f9
Ziggy Chen <ziggycs9796@hotmail.com>
2025-10-23 10:14:19 +0800

    feat: app

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -518,10 +518,10 @@ export default {
           url: this.customSubUrl
         }
       }).then(res => {
-        if (res.code === 200) {
+        if (res.data.code === 200) {
           this.$message.success('已发送至ziggy')
         } else {
-          this.$message.error('发送至ziggy失败：' + JSON.stringify(res))
+          this.$message.error('发送至ziggy失败：' + JSON.stringify(res.data))
         }
       })
     },
